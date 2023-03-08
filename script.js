4fc2fcb8080b2851d7d26e7aa1195e39d8f51701
henriquelbk <loubackhenrique123@gmail.com>
2023-03-08 20:21:12 -0300

    tentando

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const bimestre1 = [5, 7, 9.3, 5]
+const bimestre2 = [8, 6, 7.7, 9.2]
+const bimestre3 = [4.7, 6, 5.6, 6.2]
+const bimestre4 = [5, 7, 6.7, 7.7] 
+
+const notasDoAno = [bimestre1, bimestre2, bimestre3, bimestre4]
+
+for(i = 0; i < notasDoAno.length; i++){
+  
+  for(j = 0; j < bimestre1.length; j++){
+  
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const filmes = [
   {
     titulo: 'O Auto da Compadecida',
